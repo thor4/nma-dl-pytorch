fa2cd08e112b2ed803a7773eecce3139c4b159b9
Bryan Conklin <6412103+thor4@users.noreply.github.com>
2021-08-06 17:56:56 +0000

    nma week 1 update

---
+++ week1/w1d1.py
@@ -0,0 +1,11 @@
+import time
+import torch
+import random
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from torch import nn
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
